d11cd564d84cd5c602c192a5ceb61d4507b0b5f8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-23 11:11:11 +0900

    use Http class

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace digest_auth_test
     {
         static void Main(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.Write("usage: url user password");
+                Environment.Exit(1);
+            }
+
+            var url = args[0];
+            var user = args[1];
+            var password = args[2];
+
+            var response = Http.CreateRequest(url, user, password);
+            var headers = response.Headers;
+            var statuscode = (int)response.StatusCode;
+            var statusdesc = response.StatusDescription;
+            Console.WriteLine(statuscode);
+            Console.WriteLine(statusdesc);
+            Console.WriteLine(headers.ToString());
         }
     }
 }
